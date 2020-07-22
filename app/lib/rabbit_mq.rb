2125c48f7c722b9b1775ee2dbc7df82ccc3b68c7
George <archmage09@gmail.com>
2020-07-22 17:15:54 +0300

    Setup bunny geocoder service (#1)
    * Setup boilerplate
    
    * Fix typos
    
    * Add application starter
    
    * Add geocoder module from monolith
    
    * Create setting files for different env
    
    * Setup rpc via rabbitMQ
    
    * Fix typo; add correlation id to send signal to needed thread

---
+++ app/lib/rabbit_mq.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module RabbitMq
+  module_function
+
+  @mutex = Mutex.new
+
+  def connection
+    @mutex.synchronize do
+      @connection ||= Bunny.new.start
+    end
+  end
+
+  def channel
+    Thread.current[:rabbitmq_channel] ||= connection.create_channel
+  end
+
+  def consumer_channel
+    Thread.current[:rabbitmq_consumer_channel] ||= connection.create_channel(
+      nil,
+      Settings.rabbitmq.consumer_pool
+    )
+  end
+end

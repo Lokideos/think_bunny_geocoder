c122b548a873a911b664d2e7edb9ec497acb4187
lokideos <archmage09@gmail.com>
2020-08-04 14:50:15 +0300

    Add request id for updating coordinates; add option for logs debug

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -5,6 +5,13 @@ logger = Ougai::Logger.new(
   level: Settings.logger.level
 )
 
+# For debug
+# logger = Ougai::Logger.new(
+#   STDOUT,
+#   level: Settings.logger.level
+# )
+# logger.formatter = Ougai::Formatters::Readable.new
+
 logger.before_log = lambda do |data|
   data[:service] = { name: Settings.app.name }
   data[:request_id] ||= Thread.current[:request_id]

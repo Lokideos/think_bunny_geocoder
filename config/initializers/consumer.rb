1ae2a7604976c1125db06c6156b25f60e4f1db39
lokideos <archmage09@gmail.com>
2020-09-13 19:37:22 +0300

    Configure consumer to send geocode response_time metric

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -6,20 +6,22 @@ queue = channel.queue('geocoding', durable: true)
 queue.subscribe(manual_ack: true) do |delivery_info, properties, payload|
   payload = JSON(payload)
   Thread.current[:request_id] = properties.headers['request_id']
-  coordinates = Geocoder.geocode(payload['city'])
+  Application.with_time_metrics(:geocoding_request_seconds_response) do
+    coordinates = Geocoder.geocode(payload['city'])
 
-  Application.logger.info(
-    'geocoded_coordinates',
-    city: payload['city'],
-    coordinates: coordinates
-  )
+    Application.logger.info(
+      'geocoded_coordinates',
+      city: payload['city'],
+      coordinates: coordinates
+    )
 
-  if coordinates.present?
-    Metrics.geocoding_requests_total.increment(labels: { result: 'success' })
-    client = AdsService::RpcClient.fetch
-    client.update_coordinates(payload['id'], coordinates)
-  else
-    Metrics.geocoding_requests_total.increment(labels: { result: 'failure' })
+    if coordinates.present?
+      Metrics.geocoding_requests_total.increment(labels: { result: 'success' })
+      client = AdsService::RpcClient.fetch
+      client.update_coordinates(payload['id'], coordinates)
+    else
+      Metrics.geocoding_requests_total.increment(labels: { result: 'failure' })
+    end
   end
 
   channel.ack(delivery_info.delivery_tag)
